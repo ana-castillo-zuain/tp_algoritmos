14efba40e2431b660935c6d2c8c02768c33946d7
ana-castillo-zuain <164687068+ana-castillo-zuain@users.noreply.github.com>
2025-06-20 16:54:05 -0300

    ej 6 y 7

--- tp.py
+++ tp.py
@@ -250,6 +250,103 @@ while not p.pilaVacia():
     print(p.tope())
     p.desapilar()
 
+#6
+from ColaDinamica import ColaDinamica
+def retartirColaenmitades(c):
+    M1= ColaDinamica()
+    M2= ColaDinamica()
+    
+    # contar elementos 
+    contador = 0
+    aux = ColaDinamica()
+
+    while not c.colaVacia():
+        x = c.primerElemento()
+        aux.acolar(x)
+        c.desacolar()
+        contador += 1
+
+    # Calcular la mitad
+    mitad = contador // 2
+
+    for i in range(contador):
+        x = aux.primerElemento()
+        aux.desacolar()
+        c.acolar(x)
+        if i < mitad:
+            M1.acolar(x)
+        else:
+            M2.acolar(x)
+
+    return M1, M2
+
+#Ejemplo de uso
+
+# Crear una cola original
+cola = ColaDinamica()
+for elem in [10, 20, 30, 40]:
+    cola.acolar(elem)
+
+# Usar la función
+M1, M2 = retartirColaenmitades(cola)
+
+# Mostrar resultados
+print("M1:")
+while not M1.colaVacia():
+    print(M1.primerElemento())
+    M1.desacolar()
+
+print("M2:")
+while not M2.colaVacia():
+    print(M2.primerElemento())
+    M2.desacolar()
+
+
+#7
+def conjuntos_iguales(c1, c2):
+    if c1.conjuntoVacio() and c2.conjuntoVacio():
+        return True
+
+    # Crear copias de los conjuntos originales
+    copia1 = Conjunto()
+    copia2 = Conjunto()
+
+    for i in range(c1.index):
+        copia1.agregar(c1.elementos[i])
+    for i in range(c2.index):
+        copia2.agregar(c2.elementos[i])
+
+    # Comparamos los elementos de copia1 con copia2
+    if copia1.index != copia2.index:
+        return False
+
+    iguales = True
+    for i in range(copia1.index):
+        if not copia2.pertenece(copia1.elementos[i]):
+            iguales = False
+            break
+
+    return iguales
+
+# Ejemplo de uso
+a = Conjunto()
+b = Conjunto()
+
+# Agregamos elementos
+a.agregar(1)
+a.agregar(2)
+a.agregar(3)
+a.agregar(4)
+a.agregar(5)
+a.agregar(6)
+
+b.agregar(1)
+b.agregar(2)
+b.agregar(3)
+b.agregar(4)
+b.agregar(5)
+
+print(f"7) ¿Los conjuntos son iguales?: {conjuntos_iguales(a,b)}")
 
 
 #EJERCICIO 8
